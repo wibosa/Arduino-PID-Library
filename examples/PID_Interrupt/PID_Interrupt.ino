6bb2d408fde884e90ee0c65caa31715c00ce63ca
Wim Bonekamp <wim@wibosa.nl>
2018-10-10 11:40:15 +0200

    using Timertree lib for ISR
    Signed-off-by: Wim Bonekamp <wim@wibosa.nl>

--- examples/PID_Interrupt/PID_Interrupt.ino
+++ examples/PID_Interrupt/PID_Interrupt.ino
@@ -1,72 +1,70 @@
 /********************************************************
- * PID Basic Example
+ * PID timed Interrupt Basic Example ( Mega 2560 < 3kHz )
  * Reading analog input 0 to control analog PWM output 3
  ********************************************************/
+const int ledPin = LED_BUILTIN;  // the pin with a LED
 
+#include <TimerThree.h>
+
+volatile int ledState = LOW;
 #include <PID_v1.h>
-
 #define PIN_INPUT 0
 #define PIN_OUTPUT 3
-#define ledPin 13
 //Define Variables we'll be connecting to
-volatile double Setpoint, Input, Output;
-int run; // algo calcs
 //Specify the links and initial tuning parameters
+volatile double Setpoint, Input, Output;
 double Kp=12, Ki=0.001, Kd=0;
+volatile long run; // algo calcs
+
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-/* 
-Example Timer1 Interrupt
-Flash LED every second
-*/
 
-
-int timer1_counter;
-
-void setup()
-{ 
-  Serial.begin(38400);
-  //initialize the variables we're linked to
-  Input = analogRead(PIN_INPUT);
-  Setpoint = 512;
-  run = 0;
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
-   pinMode(ledPin, OUTPUT);
-
- 
-  
-
-    // initialize timer1 
-  noInterrupts();           // disable all interrupts
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1  = 0;
-
-  OCR1A = 31250/2;            // compare match register 16MHz/256/20Hz
-  TCCR1B |= (1 << WGM12);   // CTC mode
-  TCCR1B |= (1 << CS10);    // 1 prescaler 
-  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-  interrupts();             // enable all interrupts
-}
-
-
-ISR(TIMER1_COMPA_vect) { //change the 0 to 1 for timer1 and 2 for timer2
-
-   //noInterrupts();           // disable all interrupts
+void PIDcompute(void)  /// ISR
+{
+  //noInterrupts();           // disable all interrupts
   Input = analogRead(PIN_INPUT);
   myPID.Compute();
   analogWrite(PIN_OUTPUT, Output);
-  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);
+  //ledState= ledState ^ 1;
+  //digitalWrite(ledPin, ledState);
   run++;
   //interrupts();             // enable all interrupts
 }
-
-
-
+unsigned long time;
+unsigned long starttime;
+void setup()
+{ 
+  Serial.begin(115200);  
+  pinMode(ledPin, OUTPUT);
+  //Serial.print("Time: ");
+  
+  //Serial.println(time); 
+  
+  
+ 
+  //initialize the variables we're linked to
+  Input = analogRead(PIN_INPUT);
+  Setpoint = 512;
+  Output = analogRead(PIN_OUTPUT);
+  run = 0;
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+  noInterrupts();           // disable all interrupts
+  Timer3.initialize(3000);  // millisecs max FOR PID ISR about 3.3kHz better run 0.33kHz
+  Timer3.attachInterrupt(PIDcompute);
+  interrupts();             // enable all interrupts
+}
 
 void loop()
-{
+{ run=0;
+  //interrupts(); 
+  delay(995);   // display update time msecs
+  //delayMicroseconds(10000);
+  //noInterrupts(); 
+  // Serial comm 115200 takes some msecs so must be interruptable for high bandwidth
   //Serial.print("double Setpoint, Input, Output; ");
+  time = millis();
+  Serial.print(time-starttime); 
+  starttime=time;
   Serial.print(" Set ");
   Serial.print(Setpoint, DEC);
   Serial.print(" In ");
@@ -76,8 +74,6 @@ void loop()
   Serial.print(Output, DEC);
   Serial.print(" run: ");
   Serial.println(run, DEC);
-  run=0;
   
-  delay(999);
-  // your program here...
+  
 }
